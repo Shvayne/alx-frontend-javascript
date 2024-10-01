789c34304e19419ffa3d5a98c325717bb854e8b4
Christiana Apinoko <122893327+Shvayne@users.noreply.github.com>
2024-10-01 11:06:44 +0100

    remove node_moudles from git ignore

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-    let task = false;
-    let task2 = true;
+    task = false;
+    task2 = true;
   
     if (trueOrFalse) {
-      let task = true;
-      let task2 = false;
+      var task = true;
+      var task2 = false;
     }
   
     return [task, task2];
