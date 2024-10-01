d2090b6276b135f1ac6d2ad57903df4e547157a1
Christiana Apinoko <122893327+Shvayne@users.noreply.github.com>
2024-10-01 17:55:18 +0100

    add a function to iterate through a list of employee

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,7 @@
+export default function createIteratorObject(report) {
+    let allEmployees = [];
+    for (const dept of Object.values(report.allEmployees)) {
+      allEmployees = [...allEmployees, ...dept];
+    }
+    return allEmployees;
+}
