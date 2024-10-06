ff213b6552faa3cc6f061b0bee7c5d9843996871
GeebeeFrost <omgbolahan@gmail.com>
2024-10-06 05:07:24 +0000

    Lint task files

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,11 @@
-import { uploadPhoto, createUser } from "./utils";
+import { uploadPhoto, createUser } from './utils';
 
-export default function handleProfileSignup(){
-    return promise.all([uploadPhoto(), createUser()])
-        .then ((data) => {
-            console.log(
-                `${data[0].body} ${data[1].firstName} ${data[1].lastName}`,
-            );
-        })
-        .catch(() => console.log('Signup system offline'));
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((data) => {
+      console.log(
+        `${data[0].body} ${data[1].firstName} ${data[1].lastName}`,
+      );
+    })
+    .catch(() => console.log('Signup system offline'));
 }
