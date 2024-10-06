bdc64f3461b921f926c5472063a955e4e110bdec
Christiana Apinoko <122893327+Shvayne@users.noreply.github.com>
2024-10-06 17:06:50 +0100

    Create a primitive holberton class

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,18 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    if (typeof size !== 'number') {
+      throw new TypeError('Size must be a number');
+    } else if (typeof location !== 'string') {
+      throw new TypeError('Location must be a string');
+    }
+    this._size = size;
+    this._location = location;
+  }
+
+  [Symbol.toPrimitive](type) {
+    if (type === 'number') {
+      return this._size;
+    }
+    return this._location;
+  }
+}
