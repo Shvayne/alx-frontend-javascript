674be8c9a8340c964d39152cd87b1b698a84fe34
Christiana Apinoko <122893327+Shvayne@users.noreply.github.com>
2024-10-11 18:09:23 +0100

    create a teacher type

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -4,47 +4,5 @@ interface Teacher {
   fullTimeEmployee: boolean;
   yearsOfExperience?: number;
   location: string;
-  [newProp: string]: any;
+  [key: string]: any;
 }
-
-interface Directors extends Teacher {
-  numberOfReports: number;
-}
-
-interface printTeacherFunction {
-  (firstName: string, lastName: string): string;
-}
-
-const printTeacher: printTeacherFunction = (firstName, lastName) => {
-  return `${firstName[0]}. ${lastName}`;
-};
-
-interface StudentClassInterface {
-  firstName: string;
-  lastName: string;
-  workOnHomework(): string;
-  displayName(): string;
-}
-
-interface StudentClassConstructor {
-  new (firstName: string, lastName: string): StudentClassInterface;
-}
-
-const NewStudent: StudentClassConstructor = class StudentClass
-  implements StudentClassInterface
-{
-  firstName: string;
-  lastName: string;
-
-  constructor(firstName: string, lastName: string) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-  }
-
-  workOnHomework() {
-    return 'Currently working';
-  }
-  displayName() {
-    return this.firstName;
-  }
-};
