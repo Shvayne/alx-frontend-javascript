3d8bb7418ec6374c37e4a2185b2688a40a98479e
Christiana Apinoko <122893327+Shvayne@users.noreply.github.com>
2024-10-11 18:24:03 +0100

    add a student interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -18,3 +18,33 @@ interface printTeacherFunction {
 const printTeacher: printTeacherFunction = (firstname: string, lastName: string): string => {
   return `${firstname.charAt(0)}. ${lastName}`;
 };
+
+interface StudentClassInterface {
+  firstName: string;
+  lastName: string;
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+interface StudentClassConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+const NewStudent: StudentClassConstructor = class StudentClass
+  implements StudentClassInterface
+{
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework() {
+    return 'Currently working';
+  }
+  displayName() {
+    return this.firstName;
+  }
+};
