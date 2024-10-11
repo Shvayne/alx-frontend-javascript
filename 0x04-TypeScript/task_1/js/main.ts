bae0b94fa0f752467f92624013745c71012049e6
Christiana Apinoko <122893327+Shvayne@users.noreply.github.com>
2024-10-11 18:17:07 +0100

    Add printTeacher function

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -10,3 +10,11 @@ interface Teacher {
 interface Directors extends Teacher {
   numberOfReports: number;
 }
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstname: string, lastName: string): string => {
+  return `${firstname.charAt(0)}. ${lastName}`;
+};
